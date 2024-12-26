5f5cc4ec6457408ddcb26e66500a4e09edf07c15
Victor Fedyanin <fedyanin.v.v@yandex.ru>
2024-12-26 23:53:30 +0300

    FindMinimumInRotatedSortedArray

---
+++ src/binarySearch/FindMinimumInRotatedSortedArray.java
@@ -0,0 +1,57 @@
+package binarySearch;
+
+
+/*
+You are given an array of length n which was originally sorted in ascending order.
+It has now been rotated between 1 and n times. For example, the array nums = [1,2,3,4,5,6] might become:
+[3,4,5,6,1,2] if it was rotated 4 times.
+[1,2,3,4,5,6] if it was rotated 6 times.
+Notice that rotating the array 4 times moves the last four elements of the array to the beginning.
+Rotating the array 6 times produces the original array.
+Assuming all elements in the rotated sorted array nums are unique, return the minimum element of this array.
+A solution that runs in O(n) time is trivial, can you write an algorithm that runs in O(log n) time?
+
+Example 1:
+Input: nums = [3,4,5,6,1,2]
+Output: 1
+
+Example 2:
+Input: nums = [4,5,0,1,2,3]
+Output: 0
+
+Example 3:
+Input: nums = [4,5,6,7]
+Output: 4
+
+Constraints:
+1 <= nums.length <= 1000
+-1000 <= nums[i] <= 1000
+ */
+
+public class FindMinimumInRotatedSortedArray {
+    public static void main(String[] args) {
+        System.out.println(findMin(new int[] {4,5,0,1,2,3}));
+    }
+
+    public static int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int result = nums[0];
+
+        while (left <= right) {
+            if (nums[left] < nums[right]) {
+                result = Math.min(result, nums[left]);
+                break;
+            }
+
+            int mid = left + (right - left) / 2;
+            result = Math.min(result, nums[mid]);
+            if (nums[mid] >= nums[left]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return result;
+    }
+}
